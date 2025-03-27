b1a25f410a0078403edb77581ee8d945e6f87fe9
Rodrigo Casio <rod.casio@outlook.com>
2025-03-27 09:15:37 -0600

    Create blockLetters.c

---
+++ blockLetters.c
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+
+  std:: cout <<"R R R R     C C C   "<<'\n';
+  std:: cout <<"R       R  C      C    "<<'\n';
+  std:: cout <<"R       R  C        "<<'\n';
+  std:: cout <<"R R R R    C        "<<'\n';
+  std:: cout <<"R   R      C        "<<'\n';
+  std:: cout <<"R     R    C      C   "<<'\n';
+  std:: cout <<"R       R   C C C   "<<'\n';
+
+  // I like iced chai tea 
+  
+}
