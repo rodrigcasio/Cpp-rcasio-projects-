953a6159b9a39f4187f38f3f17b2155a7e0071f4
Rodrigo Casio <rod.casio@outlook.com>
2025-03-27 09:27:00 -0600

    Update quadraticFormula.cpp

--- quadraticFormula.cpp
+++ quadraticFormula.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cmath>
 
+// Here we are calculating 
+
 int main() {
   
   double a;
@@ -20,7 +22,7 @@ int main() {
   double root2;
 
 // the positive root 
-  root1 = (-b + std::sqrt(b*b - 4 * a * c)) / (2 * a);
+  root1 = (-b + std::sqrt(b*b - 4 * a * c)) / (2 * a);   
 
 // the negative root
   root2 = (-b - std::sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
@@ -28,5 +30,4 @@ int main() {
   std::cout <<"Root 1 is "<< root1 <<'\n';
   std::cout <<"Root 2 is "<< root2 <<'\n';
 
-  
 }
