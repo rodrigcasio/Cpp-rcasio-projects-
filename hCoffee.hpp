bbf48b20137a173247ba3788f7490fe4991b1435
Rodrigo Casio <rod.casio@outlook.com>
2025-04-10 17:10:14 -0600

    Create hCoffee.hpp

---
+++ hCoffee.hpp
@@ -0,0 +1,7 @@
+#include <string>
+//func decl
+std::string make_coffee(bool milk = false, bool sugar = false);
+
+//here we are using default arguments set as false. 
+//instead of typing false everytime we dont want milk or sugar..
+//we just dont type anything and 
