74ab03a7bc233543bb5cb08b172772fcd70b37c2
Rodrigo Casio <rod.casio@outlook.com>
2025-04-10 13:38:47 -0600

    Update waterPlant.cpp

--- waterPlant.cpp
+++ waterPlant.cpp
@@ -1,4 +1,19 @@
 #include <iostream>
+/*
+Define a function is_palindrome() that takes:
+
+    An std::string parameter text.
+
+The function should return:
+
+    true if text is a palindrome.
+    false if text is not a palindrome.
+
+(A palindrome is any text that has the same characters backwards as it does forwards. For example, “hannah” and “racecar” are palindromes, while “menu” and “aardvark” are not.)
+
+We will not test for edge cases such as capitalization or spaces.
+
+*/
 
 //fun dec
 std::string needs_water(int days, bool is_succulent);
@@ -21,3 +36,4 @@ std::string needs_water(int days, bool is_succulent){
     return "Don't water the plant";
   }
 }
+
