615f5ccc2b36547b6c4a27f0f5c8ffa82c7f0f2b
Rodrigo Casio <rod.casio@outlook.com>
2025-04-11 18:59:06 -0600

    Create hUfoGame.hpp

---
+++ hUfoGame.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+//fun decl:
+void display_status(std::vector <char> incorrect, std::string answer);
+void end_game(std::string answer, std::string codeword);
+void greet(void);
+void display_misses(int misses);
