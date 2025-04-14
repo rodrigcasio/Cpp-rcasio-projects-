9a609d2493e21a7b9fa088aaf6f5821ba559d067
Rodrigo Casio <rod.casio@outlook.com>
2025-04-13 20:51:34 -0600

    Update hClassConstructoEx.hpp

--- hClassConstructoEx.hpp
+++ hClassConstructoEx.hpp
@@ -1,11 +1,15 @@
 #include <string>
 
 class Song{
+  //attributes:
   std::string title;
   std::string artist;
 
 public:
+  //constructor:
+  Song(std::string new_title, std::string new_artist);
 
+  //methods used for the constructor:
   std::string get_title();
   std::string get_artist();
 };
