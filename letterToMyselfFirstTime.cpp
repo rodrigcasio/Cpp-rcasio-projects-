8d85623ea01295937f73d84ce9908032bbdb0aaa
Rodrigo Casio <rod.casio@outlook.com>
2025-04-16 14:27:45 -0600

    Create letterToMyselfFirstTime.cpp

---
+++ letterToMyselfFirstTime.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Dear Self,\n";
+  
+  std:: cout <<"It is 1:12 am, and im using Codecadamy for the first time. I dont know where this will take me, but I'm sure it will be useful if I want to become a great programer. I hope you keep going and you achieve your goals. See you in the near future."<<'\n';
+
+  std:: cout <<"Rod";
+  
+  
+  
+}
