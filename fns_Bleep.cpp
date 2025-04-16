b7b6580d1c8ec02068662ca65c0a64fba7b578c3
Rodrigo Casio <rod.casio@outlook.com>
2025-04-16 14:42:41 -0600

    Create fns_Bleep.cpp

---
+++ fns_Bleep.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include "hBleep.hpp"
+
+void asterisks(std::string word, std::string &text, int i){
+  for(int n = 0; n < word.length(); n++){
+    if(i + n < text.length()){ // making sure we dont go over the amount of characters of text
+      text[i+n] = '*'; //same starting point of text i from the outer loop of bleep + the amount of characters of the word.
+    }
+}
+
+void bleep(std::string word, std::string &text){
+  for(int i = 0; i <= text.length(); i++){ // loops through the text
+    int matchCounter = 0;  //create a counter to compare the size of word later 
+    for(int j = 0; j < word.length(); j++){  
+      if(i + j >= text.length() || text[i+j] != word[j]){//check if the addition of i + j is greater than total amount, then compares the characters.
+        break; //if 1 condtions is true, stops.
+      }
+      matchCounter++;
+    }
+    if(matchCounter == word.length()){ //if match == to 8 characters, (8  charaters = broccoli)..
+      asterisks(word, text, i);
+    }
+  }
+}
