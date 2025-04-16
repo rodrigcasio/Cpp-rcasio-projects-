cceafd9c1569eff4b736b6de605f7d4e55135fe5
Rodrigo Casio <rod.casio@outlook.com>
2025-04-16 14:31:54 -0600

    Create h.Bleep.cpp

---
+++ h.Bleep.cpp
@@ -0,0 +1,10 @@
+#include <string>
+
+//fun declr
+
+//function to change the characters in the text into asterisk at position of the outer loop "i" 
+void asterisks(std::string word, std::string &text, int i);
+
+//function to check each characters and comparing them to the word assigned.
+void bleep(std::string word, std::string &text);
+
