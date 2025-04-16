8fbbffdd2f37850d5a04a14b46472dcdb723a6b4
Rodrigo Casio <rod.casio@outlook.com>
2025-04-16 14:30:01 -0600

    Create mainBleep.cpp

---
+++ mainBleep.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+#include "hbleep.hpp"
+#include <string>
+
+int main(){
+
+  std::string word = "broccoli";
+  std::string text = "I like broccoli, broccoli is good for your health";
+
+  return 0;
+}
