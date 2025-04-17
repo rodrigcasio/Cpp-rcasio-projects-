cdaf4a79408a88c5c29253071e2d7a3ac8b183c8
Rodrigo Casio <rod.casio@outlook.com>
2025-04-16 20:24:29 -0600

    Create h_IsValidSetterEx.hpp

---
+++ h_IsValidSetterEx.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void isValid(int code, int speed){
+  if(code == 1){
+    std::cout << "Car speed succesfully set to" << speed << std::endl;
+  }
+  else if(code == 2){
+    std::cout <<"Car speed is invalid! Speed is set to" << speed <<'!'<< std::endl;
+  }
+  else{
+    std::cout << "Car is too fast! Speed is set to " << speed << '!' << std::endl;
+  }
+}
