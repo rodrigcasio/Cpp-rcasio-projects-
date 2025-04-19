22a6d450b5cf4a981376c1df5299af0839648339
Rodrigo Casio <rod.casio@outlook.com>
2025-04-18 21:34:50 -0600

    Update fns_BankAccount.cpp

--- fns_BankAccount.cpp
+++ fns_BankAccount.cpp
@@ -1,19 +1,18 @@
 #include <string>
 #include "h_BankAccount.hpp"
-using namespace std;
 
 //constructor BankAccount:
-BankAccount::BankAccount(string newAccountNumber, double newBalance): accountNumber(newAccountNumber), balance(newBalance) {}
+BankAccount::BankAccount(std::string newAccountNumber, double newBalance): accountNumber(newAccountNumber), balance(newBalance) {}
 
 //methods BankAccount class:
-string BankAccount::getAccountNumber(){
+std::string BankAccount::getAccountNumber(){
   return accountNumber;
 }
 double BankAccount::getBalance(){
   return balance;
 }
 
-void BankAccount::setAccountNumber(string new_account_number){
+void BankAccount::setAccountNumber(std::string new_account_number){
   accountNumber = new_account_number;
 }
 void BankAccount::setBalance(double new_balance){
@@ -26,29 +25,29 @@ BankAccount::Transaction::Transasction(BankAccount& acc): account(acc) {}
 //methods Transaction: 
 void BankAccount::Transaction::setDeposit(double new_deposit){
   if(new_deposit <= 0){
-    cout << "*Invalid  deposit : Amount must be positive*" << endl << endl;
+    std::cout << "*Invalid  deposit : Amount must be positive*" << std::endl << std::endl;
     return;
   }
   if(new_deposit > 1000000){
-    cout << "*Invalid deposit : Exceeds the maximum limit*" << endl << endl;
+    std::cout << "*Invalid deposit : Exceeds the maximum limit*" << std::endl << std::endl;
     return;
   }
   account.setBalance(account.getBalance() + new_deposit);
-  cout <<"Your deposit was done succesfully" << endl;
-  cout <<"New Balance: $" << account.getBalance() << endl << endl;
+  std::cout <<"Your deposit was done succesfully" << std::endl << std::endl;
+  std::cout <<"New Balance: $" << account.getBalance() << std::endl << std::endl;
 }
 
 void BankAccount::Transaction::setWithdraw(double new_withdraw){
   if(new_withdraw <= 0){
-    cout <<"*Invalid withdrawl : Amount must be positive*" << endl << endl;
+    std::cout <<"*Invalid withdrawl : Amount must be positive*" << std::endl << std::endl;
     return false; 
   }
   if(account.getBalance() < new_withdraw){
-    cout <<"*Invalid withdrawl : not enough funds*" << endl <, endl;
+    sdt::cout <<"*Invalid withdrawl : not enough funds*" << std::endl < std::endl;
     return false;
   }
   account.setBalance(account.getBalance() - new_withdraw);
-  cout <<"Your withdrawl was done successfully" << endl;
-  cout <<"New balance: $" << account.getBalance() << endl << endl;
+  std::cout <<"Your withdrawl was done successfully" <<std::endl;
+  std::cout <<"New balance: $" << account.getBalance() << std::endl << std::endl;
   return true;
 }
