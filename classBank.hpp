4e07edd4530d5ad992e161ab912e30dae443caaf
Rodrigo Casio <rod.casio@outlook.com>
2025-04-21 10:44:03 -0600

    Update classBank.hpp

--- classBank.hpp
+++ classBank.hpp
@@ -24,7 +24,51 @@ public:
     }
     return nullptr;
   }
-  
+
+  int transferAccount(BankAccount& sourceAccount, BankAccount& depositAccount, double amount){
+    int statusCode;
+    BankAccount* sourcePtr = findAccount(sourceAccount.getAccountNumber()); //get the address of the object BankAccount class.
+    BankAccount* depositPtr = findAccount(depositAccount.getAccountNumber());
+    if(sourcePtr != &sourceAccount || depositPtr != &depositAccount){ //check if pointers are pointing to the same object.
+      std::cout <<"*Invalid transfer: accounts not found*" << std::endl;
+      std::cout <<"Status Code: ";
+      return statusCode = 2; 
+    }
+    if(sourcePtr->getAccountNumber() == depositPtr->getAccountNumber()){ //prevents transfer to same accounts.
+      std::cout <<"*Invalid transfer: cannot deposit from same account*"<< std::endl;
+      std::cout <<"Status code: ";
+      return statusCode = 2;
+    }
+    if(amount <= 0){ //1st amount condition
+      std::cout<<"*Invalid transfer: amount should be a positive*"<< std::endl;
+      std::cout <<"Status Code: ";
+      return statusCode = 3;
+    }
+    
+    if(amount <= sourcePtr->getBalance()){ //2nd amount condition
+      //initialize an object from Transaction class with its constructor, patameters used is dereference of the pointer.
+      BankAccount::Transaction sourceAcc(*sourcePtr); //dereference sourcePtr.
+      if(!sourceAcc.setWithdraw(amount)){ //setWithdraw return bool. If condition true,  transaction is not done.
+        std::cout <<"*Invalid Transfer: transaction could not be processed*" << std::endl;
+        std::cout <<"Status Code: ";
+        return statusCode = 3;
+      }
+      
+      BankAccount::Transaction depositAcc(*depositPtr); //initialize the constructor of Transacion class to dereference depositPtr.
+      if(!depositAcc.setDeposit(amount)){
+        std::cout <<"*Invalid transfer: transaction could not be processed*"<< std::endl;
+        std::cout <<"Status Code: ";
+        return statusCode = 3;
+      }
+      //good to go. 
+      std::cout <<"Your transfer was processed successfully"<< std::endl;
+      std::cout <<"Status Code: ";
+      return statusCode = 1;
+    }
+    std::cout <<"*Invalid transfer: insufficient funds"* << std::endl;
+    std::cout <<"Status Code: ";
+    return statusCode = 3;
+  }
 };
 
 #endif
