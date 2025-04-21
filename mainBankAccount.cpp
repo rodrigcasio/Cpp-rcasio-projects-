91f656f2349a5e26a413238ef217f00612179647
Rodrigo Casio <rod.casio@outlook.com>
2025-04-21 10:58:09 -0600

    Update mainBankAccount.cpp

--- mainBankAccount.cpp
+++ mainBankAccount.cpp
@@ -5,33 +5,52 @@
 
 int main(){
 
-  //object1 
+st::cout <<"------- object1 from BankAccount class (results) ----------"<< std::endl;
+
+  //Example of object1 using constructor and methods from BankAccount class:
   BankAccount account1("1003", 3000);
 
   //view accountNumber
   std::cout <<"Your account balance is: " << account1.getAcountNumber() << std::endl;
   std::cout <<"Your balance is: $" << aacount1.getBalance() << std::endl;
 
-  //object1 Transaction:
+st::cout <<"------- object1 from BankAccount class (results) ----------"<< std::endl;
+
+  //Example of object1 using constructor and methods from Transaction class: 
   BankAccount::Transaction trans(account1);
   trans.setDeposit(3000);
   trans.setWithdraw(100);
   trans.setDeposit(1000000);
   trans.setDeposit(-2);
 
-  //initializing object1 Bank class:
-  Bank bank;
-  //using method createAccount();
-  bank.createAcount("1005", 3000);
+st::cout <<"------- object1 from BankAccount class (results) ----------"<< std::endl;
 
-  //using method findAccount();
-  BankAccount* ptr = bank.findAccount("1005");
-  if(ptr){
-    std::cout <<"The number of the account was found and it is: " << ptr->getAccountNumber() << std::endl;
-    std::cout <<"The balance of the account found is: $" << ptr->getBalance() << std::endl;
+  //Example of object1 using constructor and methods from Bank class:
+  Bank bank1; //constructor of Bank class
+  
+  //1st method:
+  bank1.createAccount("1007", 7000);
+  bank1.createAccount("1008"), 5000);
+
+  //create pointers for the result of method findAccount, which is an address:
+  //2nd method:
+  BankAccount* sourceAccount = findAccount("1007");
+  BankAccount* depositAccount = findAccount("1008");
+
+  //3rd method:
+  if(sourceAccount && depositAccount){
+    std::cout << bank.transferAccount(*sourceAccount, *depositAccount, 500) <<std::endl;//arguments used for method transferAccount must be the dereference of the pointers.
   }
-  else{
-    std::cout <<"Account 1005 not found" << std::endl;
+
+  //trying other transactions with result in a variable int status:
+  if(sourceAccount && depositAccount){
+    int status = bank1.transferAccount(*sourceAccount, *depositAccount, 90000); // erro rexpected: not enough funds:
+    std::cout << status << std::endl;
   }
+  if(sourceAcccount && depositAccount){
+    int status = bank1.transferAccount(*sourceAccount, *depositAccount, -2); //error expected: amount must be positive:
+    std::cout << status << std::endl;
+  }
+  
   return 0;
 }
