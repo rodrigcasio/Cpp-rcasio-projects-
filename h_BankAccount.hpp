8ed5c6b4d5259b5f539d2a30b578b69794645a35
Rodrigo Casio <rod.casio@outlook.com>
2025-04-22 23:12:08 -0600

    Update h_BankAccount.hpp

--- h_BankAccount.hpp
+++ h_BankAccount.hpp
@@ -1,5 +1,5 @@
-#ifndef #H_BANKACCOUNT_HPP
-#define #H_BANKACCOUNT_HPP
+#ifndef H_BANKACCOUNT_HPP
+#define H_BANKACCOUNT_HPP
 
 #include <string>
 #include <iostream>
@@ -7,36 +7,37 @@
 
 
 class BankAccount{
-  std::string accountNumber;
-  double balance;
-  std::vector <std::string> transactionList;
-  double interestRate;
-  bool isSavings;
+	std::string accountNumber;
+	double balance;
+	std::vector <std::string> transactionList;
+	double interestRate;
+	bool isSavings;
 
 public:
-  class Transaction{
-    BankAccount& account;
-  public:
-    Transaction(BankAccount& acc); //constructor of reference BA&
-    void setDeposit(double new_deposit);
-    bool setWithdraw(double new_withdraw);
-    bool applyInterest();
-  };
 
-  BankAccount(std::string newAccount, double newBalance, double newInterestRate = 0.0, bool isSavings = false); //constructor
-  //getter methods:
-  string getAccountNumber();
-  std::string getTransactionInfo();
-  double getBalance();
-  double getInterestRate();
-  bool isSavingsAccount();
-  //setter methods:
-  void setAccountNumber(std::string new_account_number);
-  void setBalance(double new_balance);
-  void setTransactionInfo(const std::string& transactionInfo);
-  void setInterestRate(double newRate);
-  
-  
+	class Transaction{
+		BankAccount& account;
+
+	public:
+		Transaction(BankAccount& acc); //constructor of reference BA&
+		bool setDeposit(double new_deposit);
+		bool setWithdraw(double new_withdraw);
+		bool applyInterest();
+	};
+
+	BankAccount(std::string newAccount, double newBalance, double newInterestRate = 0.0, bool savings = false); //constructor 
+	//getter methods:
+	std::string getAccountNumber();
+	std::string getTransactionInfo();
+	double getBalance();
+	double getInterestRate();
+	bool isSavingsAccount();
+
+	//setter methods:
+	void setAccountNumber(std::string new_account_number);
+	void setBalance(double new_balance);
+	void setTransactionInfo(const std::string& transactionInfo);
+	void setInterestRate(double newRate);
 };
 
 #endif
