b7b56979952320dbfcdff23d4c84b5b28a19a320
Rodrigo Casio <rod.casio@outlook.com>
2025-04-24 00:10:16 -0600

    Create h_AnimalPrivateEx.hpp

---
+++ h_AnimalPrivateEx.hpp
@@ -0,0 +1,23 @@
+#ifndef ANIMAL_PRIVATE_EX_HPP
+#define ANIMAL_PRIVATE_EX_HPP
+
+#include <iostream>
+#include <string>
+
+class Animal{
+public:
+  std::string species;
+  std::string name;
+
+  void eat();
+  std::string getName();
+
+protected:
+  int age;
+  void sleep();
+
+private:
+  void makeSound();
+};
+
+#endif 
