f080741578a2b9adad5e8ed43ce087fe6558318c
Rodrigo Casio <rod.casio@outlook.com>
2025-04-24 17:06:40 -0600

    Create fns_BirdMultipleInheEx.cpp

---
+++ fns_BirdMultipleInheEx.cpp
@@ -0,0 +1,12 @@
+#include "h_BirdMultipleInheEx.hpp"
+#include <iostream>
+
+void Bird::fly(){
+  std::cout <<"flying at "<< maxHeight <<"above sea level "<< std::endl;
+}
+void Bird::land(){
+  std::cout <<"Time to land!"<< std::endl;
+}
+void Bird::setMaxHeight(int animalHeight){
+  maxHeight = animalHeight;
+}
