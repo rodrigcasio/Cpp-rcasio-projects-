725cac4d4ff2dcaff3344a25dbbf642e971975e5
Rodrigo Casio <rod.casio@outlook.com>
2025-04-24 20:12:03 -0600

    Create mainConstructorInheEx.cpp

---
+++ mainConstructorInheEx.cpp
@@ -0,0 +1,10 @@
+#include "h_LatteConstructorInheEx.hpp"
+#include <iostream>
+
+int main(){
+
+  Latte smallLatte("Small Latte", "10oz");
+  Latte generalLatte;   //uses the Coffee(); constructor and Latte(); constructor;
+  
+  return 0;
+}
