b0f47b1d85f322117c9457cf6da1005a97aa17b5
Rodrigo Casio <rod.casio@outlook.com>
2025-04-25 12:15:21 -0600

    Create mainDiamondProbMultInheEx.cpp

---
+++ mainDiamondProbMultInheEx.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "h_HybridCarDiamondMultiInheEx.hpp"
+
+int main(){
+
+  //initializing constructor of Hybrid class. consecuentially will create subobjects of the rest of the derived classes...
+  HybridCar hybrid;
+  hybrid.drive();
+
+  return 0;
+}
+
+/*
+When an object of a derived class is created, subobjects of the base class are also created. 
+In our example, when we created an object of type ThirdDerivedClass (ThirdDerivedClass tdc;), 
+subobjects of SecondDerivedClass and FirstDerivedClass were also created, 
+which themselves created two subobjects of BaseClass.
+
+When someMethod() (a member of BaseClass) was called by an object of ThirdDerivedClass,
+it was unclear which one of the two BaseClass subobjects to use to call someMethod().
+
+1. We can correct this problem by explicitly specifying which BaseClass subobject to use by using the scope resolution operator (::) like so:
+
+2. Using virtual in this way ensures only one BaseClass subobject is created and is shared among FirstDerivedClass and SecondDerivedClass. 
+We are required to use virtual in both derived classes, or else we will get ambiguity errors again.
+
+how to compile: 
+
+g++ mainDiamondMultiInheEx.cpp fns_VehicleDiamondMultiInheEx.cpp fns_CarDiamondMultiInheEx.cpp 
+fns_TruckDiamondMultipleInheEx.cpp fns_HybridCarDiamondMultiInheEx.cpp 
+
+
+*/
