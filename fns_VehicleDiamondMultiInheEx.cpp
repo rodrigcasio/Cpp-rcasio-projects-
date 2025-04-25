b7e255cfc3a2f4dc2768e0201dde0a124cc8b67a
Rodrigo Casio <rod.casio@outlook.com>
2025-04-25 12:15:28 -0600

    Create fns_VehicleDiamondMultiInheEx.cpp

---
+++ fns_VehicleDiamondMultiInheEx.cpp
@@ -0,0 +1,10 @@
+#include "h_VehicleDiamondMultiInheEx.hpp"
+
+Vehicle::Vehicle(){
+  std::cout << "Vehicle constructor called" << std:endl;
+}
+
+void Vehicle::drive(){
+  std::cout << " " << std::endl;
+  std::cout <<"Driving the vehicle." << std::endl;
+}
