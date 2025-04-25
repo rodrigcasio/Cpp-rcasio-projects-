f5d43b4da8ed3eb6f65effd947d6843e0da4eb2b
Rodrigo Casio <rod.casio@outlook.com>
2025-04-25 12:15:24 -0600

    Create h_VehicleDIamondMultiInheEx.hpp

---
+++ h_VehicleDIamondMultiInheEx.hpp
@@ -0,0 +1,13 @@
+#ifndef VEHICLE_DIAMOND_MULTIPLE_INHE_EX_HPP
+#define VEHICLE_DIAMOND_MULTIPLE_INHE_EX_HPP
+#include <iostream>
+
+class Vehicle{
+Public:
+  Vehicle(); //constructor
+  void drive();
+
+};
+
+
+#endif 
