7f0605585261fe048477495b20f8e42edd2b8fa7
Rodrigo Casio <rod.casio@outlook.com>
2025-04-24 18:18:54 -0600

    Create h_GermanshepherdMultilevelInheEx.hpp

---
+++ h_GermanshepherdMultilevelInheEx.hpp
@@ -0,0 +1,12 @@
+#ifndef GERMANSHEPHERD_MULTILEVEL_INHE_HPP
+#define GERMANSHEPHERD_MULTILEVEL_INHE_HPP
+#include "h_DogMultilevelInheEx.hpp"
+
+class GermanShepherd : public Dog {
+public: 
+  void guard();
+  void printInfo();
+
+};
+
+#endif 
