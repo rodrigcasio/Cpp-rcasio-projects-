285297a4b6786906ae7aec3ea3ec5d940612c0a1
Rodrigo Casio <rod.casio@outlook.com>
2025-04-25 20:34:04 -0600

    Create fns_JapaneseRestaurant.cpp

---
+++ fns_JapaneseRestaurant.cpp
@@ -0,0 +1,15 @@
+#include "h_JapaneseRestaurant.hpp"
+#include <iostream>
+
+JapaneseRestaurant::JapaneseRestaurant(std::string n, std::string addr, bool tatamiAvailable)
+									  : Restaurant(n, addr), hasTatami(tatamiAvailable) {}
+
+bool JapaneseRestaurant::getTatamiAvailable(){
+	return hasTatami;
+}
+void JapaneseRestaurant::describeCuisine(){
+	if(!getTatamiAvailable()){
+		std::cout << "Traditional Japanese cuisine" << std::endl << std::endl;
+	}
+	std::cout <<"Traditional Japanese cuisine tatami seating available" << std::endl << std::endl;
+}
