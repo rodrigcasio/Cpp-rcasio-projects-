5842128dd397c1db97335cf00d360648dd0e3229
Rodrigo Casio <rod.casio@outlook.com>
2025-04-26 22:56:38 -0600

    Create fns_CarSpecifierOverride.cpp

---
+++ fns_CarSpecifierOverride.cpp
@@ -0,0 +1,9 @@
+#include "h_CarSpecifierOverrideEx.hpp"
+#include <iostream>
+
+void Car::setSpeed(int newSpeed){
+  if(newSpeed > 50){
+    std::cout << "Car is moving fast!" << std::endl;
+  }
+  speed = newSpeed;
+}
