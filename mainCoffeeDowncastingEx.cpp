5ff5589dfb97fe89f88ed7aa6079dde5791419f0
Rodrigo Casio <rod.casio@outlook.com>
2025-04-27 20:33:53 -0600

    Create mainCoffeeDowncastingEx.cpp

---
+++ mainCoffeeDowncastingEx.cpp
@@ -0,0 +1,26 @@
+#include "h_CoffeesDowncastingEx.hpp"
+#include <iostream>
+#include <vector>
+
+int main(){
+
+  std::vector <Coffee*> coffeeCups = createCoffeeCups();
+  for(int i = 0; i < coffeeCups.size(); i++){
+    Coffee* cup = coffeeCups[i];
+    SpecialBlend* specialCup = dynamic_cast<SpecialBlend*>(cup);
+    if(specialCup != nullptr){
+      std::cout << "Coffee " << i << " is a special blend from " << specialCup->getLocation() << std::endl; 
+    }else{
+      std::cout << "Coffee " << i << " is not a special blend!" << std::endl;
+    }
+  }
+  
+  return 0;
+}
+
+/*
+  How to run:
+  g++ mainCoffeeDowncasting.cpp fns_ColdBrewDowncasting.cpp fns_EspressoDowncastingEx.cpp 
+  fns_SpecialBlendDowncastingEx.cpp -o coffee
+  ./coffee
+*/
