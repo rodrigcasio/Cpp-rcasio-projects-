317b9e4c9d1b04152e136e212f4c606294fef344
Rodrigo Casio <rod.casio@outlook.com>
2025-04-27 20:36:10 -0600

    Create fns_ColdBrewDowncastingEx.cpp

---
+++ fns_ColdBrewDowncastingEx.cpp
@@ -0,0 +1,5 @@
+#include "h_ColdBrewDowncastingEx.hpp"
+
+ColdBrew::ColdBrew(int cSize, int cAmount): Coffee(cSize, cAmount, false) {}
+
+//false because its a cold drink, following the constructor of coffee
