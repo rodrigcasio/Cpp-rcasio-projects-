6a219d9c60fa4614b94543a6542602a1fb88fac0
Rodrigo Casio <rod.casio@outlook.com>
2025-04-29 16:15:15 -0600

    Create h_CharacterDuel.hpp

---
+++ h_CharacterDuel.hpp
@@ -0,0 +1,17 @@
+#ifndef CHARACTER_DUEL_HPP
+#define CHARACTER_DUEL_HPP
+
+class Character{
+  public:
+    Character(std::string cName = "Elden King", int cHealth = 100, int cPower = 50);
+    virtual void attack();
+    virtual void displayInfo();
+
+  protected:
+    std::string name;
+    int health;
+    int power;
+};
+
+
+#endif 
