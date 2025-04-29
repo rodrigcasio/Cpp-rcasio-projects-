c7ce51110d749a1d4de0ba4cb8bf69b99d4ae0f3
Rodrigo Casio <rod.casio@outlook.com>
2025-04-28 21:14:06 -0600

    Create h_AnimalPolyReviewEx.hpp

---
+++ h_AnimalPolyReviewEx.hpp
@@ -0,0 +1,11 @@
+#ifndef ANIMAL_POLY_REVIEW_EX_HPP
+#define ANIMAL_POLY_REVIEW_EX_HPP
+#include <iostream>
+
+class Animal {
+  public:
+    virtual void makeSound();
+    virtual ~Animal();
+};
+
+#endif
