9620745c63d011d30fc9b0bb6395f0a29bfbfffd
Rodrigo Casio <rod.casio@outlook.com>
2025-04-30 17:59:26 -0600

    Create h_FlyingCarIterfacesEx.hpp

---
+++ h_FlyingCarIterfacesEx.hpp
@@ -0,0 +1,17 @@
+#include "h_CarInterfacesEx.hpp"
+#include "h_AircraftInterfacesEx.hpp"
+
+class FlyingCar: public Aircraft: public Car{
+  public:
+    void drive(int carSpeed) override;
+    void stop() override;
+    void fly(int aircraftHeight) override;
+    void land();
+
+    int getCarSpeed;
+    int getCarHeight;
+
+  private:
+    int speed;
+    int height;
+};
