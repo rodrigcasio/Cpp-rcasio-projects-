2c49d0793ef47a0fdb0ec46460a659726e5e86b4
Rodrigo Casio <rod.casio@outlook.com>
2025-05-05 19:00:43 -0600

    Create h_CounterStorageClassesEx.hpp

---
+++ h_CounterStorageClassesEx.hpp
@@ -0,0 +1,9 @@
+//func def
+// declaration of extern varaible:
+extern int externCounter;
+
+//Function to demonstrate static variable behavior
+void incrementStatiCounter();
+
+//Funtion to demonstrate auto variable behavior
+void incrementAutoCounter();
