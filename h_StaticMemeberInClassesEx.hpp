ddf7db4c8ad5b0d3b9d0c81ebd313d73543d9be3
Rodrigo Casio <rod.casio@outlook.com>
2025-05-06 22:25:27 -0600

    Create h_StaticMemeberInClassesEx.hpp

---
+++ h_StaticMemeberInClassesEx.hpp
@@ -0,0 +1,14 @@
+#include <string>
+
+class Player{
+  public:
+    static int playerCount;               //static member variable
+    Player(std::string playerName);
+    void setPlayerName(std::string newName);
+    std::string getPlayerName();
+    void displayPlayerInfo();
+    static void displayPlayerCount();    //static function
+
+  private:
+    std::string name;
+};
