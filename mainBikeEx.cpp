d96199a0e6d88ff9bf6f0b18a13e0555bb50484f
Rodrigo Casio <rod.casio@outlook.com>
2025-05-07 19:47:07 -0600

    Update mainBikeEx.cpp

--- mainBikeEx.cpp
+++ mainBikeEx.cpp
@@ -4,6 +4,17 @@
 int main(){
 
   Bike myBike("Mqueen", 20, 80, 30);
+
+  //display bike information:
+  myBike.displayBikeInfo();
+
+  //changing the cadence of myBike
+  myBike.changeBikeCadence(90);
+
+  //chaning the pace of myBike
+  myBike.chaningBikePace(10);
+
+  //diplaying the information with new changes
   myBike.displayBikeInfo();
 
   return 0;
