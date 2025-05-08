b087ced8bec638c9829aee0dbb7919cf8dc19fcf
Rodrigo Casio <rod.casio@outlook.com>
2025-05-07 19:10:44 -0600

    Create fns_ExternLinkageEx.cpp

---
+++ fns_ExternLinkageEx.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int score = 100;         //defining the global variable:
+
+void displayScore(){        //defining the global function:
+  std::cout << "Score: " << score << std::endl;
+}
