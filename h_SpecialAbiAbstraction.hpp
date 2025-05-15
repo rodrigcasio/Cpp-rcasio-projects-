d618ce3011c2709676735266192ee86dab249654
Rodrigo Casio <rod.casio@outlook.com>
2025-05-14 19:49:38 -0600

    Create h_SpecialAbiAbstraction.hpp

---
+++ h_SpecialAbiAbstraction.hpp
@@ -0,0 +1,9 @@
+#ifndef SPECIAL_ABI_ABSTRACTION_HP
+#define SPECIAL_ABI_ABSTRACTION_HPP
+//inteface class
+class SpecialAbility{
+  public:
+    virtual void doTrick() = 0;     //pure virtual function 
+};
+
+#endif
