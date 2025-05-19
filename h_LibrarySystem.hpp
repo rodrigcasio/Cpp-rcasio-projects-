fbdf2b0cde343f4ebc72c459c7dc0e9bcf5c402f
Rodrigo Casio <rod.casio@outlook.com>
2025-05-18 22:46:08 -0600

    Update h_LibrarySystem.hpp

--- h_LibrarySystem.hpp
+++ h_LibrarySystem.hpp
@@ -1,12 +1,24 @@
 #include <vector>
 #include <string>
 
+extern double libraryBudget;
+
 class Library {
   public:
-    void addBook(std::string newBook);
+    Library();
+    int getReservedCount() const;
+
+    void addBook(std::string newBook, double bookCost);
     void borrowBook(std::string bookTitle);
     void listBooks() const;
+    static int getTotalBorrowedBooks();
+    static void getTotalBorrowedBooks2();
+    void reserveBook(std::string title) const;
 
   private:
-  std::vector<std::string> bookList;
+    std::vector<std::string> bookList;
+    static int totalBorrowedBooks;
+    mutable int reservedCount;        //modified in reserveBook() const func
+    mutable std::vector<std::string> reservedBooks;  //modified in reserveBook() func
+  
 };
