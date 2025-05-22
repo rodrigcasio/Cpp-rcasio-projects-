f58120b91a430e920bb6a2f6711001fcb9da51c9
Rodrigo Casio <rod.casio@outlook.com>
2025-05-22 17:48:21 -0600

    Update h_LibrarySystem.hpp

--- h_LibrarySystem.hpp
+++ h_LibrarySystem.hpp
@@ -3,22 +3,27 @@
 
 extern double libraryBudget;
 
-class Library {
-  public:
-    Library();
-    int getReservedCount() const;
+class Library{
+    public:
+        Library();
+        int getReservedCount() const;
+        void addBook(std::string newBook, double bookCost);
+        bool borrowBook(std::string bookTitle);
+        void listBooks() const;
+        static int getTotalBorrowedBooks();
+        static void getTotalBorrowedBooks2();
+        void reserveBook(std::string title) const;
+        void setLibraryMember(std::string memberName);  // new
+        void memberList() const;
+        static int getTotalMembersCount();
+        static void displayMembersNumber();
 
-    void addBook(std::string newBook, double bookCost);
-    void borrowBook(std::string bookTitle);
-    void listBooks() const;
-    static int getTotalBorrowedBooks();
-    static void getTotalBorrowedBooks2();
-    void reserveBook(std::string title) const;
+    private:
+        std::vector<std::string> bookList;
+        static int totalBorrowedBooks;
+        mutable int reservedCount;
+        mutable std::vector<std::string> reservedBooks;
+        static int totalMembersCount;
+        std::vector<std::string> libraryMembers;
 
-  private:
-    std::vector<std::string> bookList;
-    static int totalBorrowedBooks;
-    mutable int reservedCount;        //modified in reserveBook() const func
-    mutable std::vector<std::string> reservedBooks;  //modified in reserveBook() func
-  
 };
