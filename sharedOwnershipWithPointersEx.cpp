a767b095df0a3a3b240d6a126f09553d53b2a85e
Rodrigo Casio <rod.casio@outlook.com>
2025-06-06 18:01:58 -0600

    Update sharedOwnershipWithPointersEx.cpp
    After understanding better what reset() does to a shared_ptr, I changed the organization of the code explaining two examples of using shared_ptr and their behavior

--- sharedOwnershipWithPointersEx.cpp
+++ sharedOwnershipWithPointersEx.cpp
@@ -3,19 +3,34 @@
 
 int main(){
 
-  //1. creating a smart shared pointers:
+  // ===== 1st example:   Removing ownerships of shared_ptr's sharing the same allocated integer on the heap.
+  
   std::shared_ptr<int> ptr1 = std::make_shared<int>(7);
   std::shared_ptr<int> ptr2 = ptr1;
   std::shared_ptr<int> ptr3 = ptr1;
 
-  std::cout << ptr.use_count() << std::endl;               //shows the number of shared pointing to the same resource outputs 3
+  std::cout << "-total # of pointers owning \"ptr\" same resource (including ptr): " << ptr.use_count() << std::endl;               //shows the number of shared pointing to the same resource outputs 3
 
-    //deleting one of them with reset() method:
-  ptr3.reset();
+  ptr3.reset();     // reset()
+                    // removes ownership of the integer variable on the heap.
+                    // It is not pointing to any resource on the heap.
+                    // ptr3 becomes an empty pointer, but the variable
+                    // pointer is still allocated on the stack
+  std::cout << "\t--\"ptr3\" ownership gets removed" << std::endl;
+  
+  std::cout << "-total # of pointers owning \"ptr\" same resource (including ptr): " << ptr.use_count() << std::endl;       // outputs 2
 
-  std::cout << ptr.use_count() << std::endl;      //outputs 2 
+  ptr.reset();         // removing "ptr" ownership 
+  
+  std::cout << "\t--\"ptr\" ownership gets removed" << std::endl << std::endl;
+  
+  // ptr2 is the last pointer that owns the variable integer (7) 
+  std::cout << "-total # of pointers owning \"ptr2\" same resource (including ptr2): " << ptr2.use_count() << std::endl;      // output 1
+  std::cout << "-Resource owned by \"ptr2\": " << *ptr2 << std::endl;
 
-  // ==== 2nd example:
+  std::cout << "\t------" << std::endl << std::endl; 
+
+  // ==== 2nd example:       outputing the address of the integer variable allocated on the heap, by using get() on the pointers which own the resource.
 
   std::shared_ptr<int> firstPtr = std::make_shared<int>(300);
   std::shared_ptr<int> secondPtr = firstPtr;
