c0bb47da4876bd0cd182773c0ae49594aa803734
Rodrigo Casio <rod.casio@outlook.com>
2025-06-09 21:08:14 -0600

    Create fns_RestaurantMemoryManagement.cpp

---
+++ fns_RestaurantMemoryManagement.cpp
@@ -0,0 +1,25 @@
+#include "h_RestaurantMemoryManagement.hpp"
+#include <iostream>
+
+Restaurant::Restaurant(int initialTableCount): tableCount(initialTableCount) {  // 6. Define Constructor 
+  for(int i = 1; i <= initialTableCount; ++i){
+    tables.push_back(std::make_unique<Table>(i));  // 6. create a table, adding each table to "tables: vector
+                                                    // create table with std::make_unique<Table>(i);
+                                                    // add each table to "tables" vector with "tables.push_back(std::make_unique<Table>(i);"; 
+  }
+}
+
+bool Restaurant::reserveTable(const std::shared_ptr<Customer>& customer){  // 8. Takes a shared_ptr& to a Customer (a shared pointer can only be passed on by reference)
+  for(const std::unique_ptr<Table>& table : tables){
+    if(table->getIsAvailable()){            // 9. accessing getIsAvailable through a pointer with "table->getIsAvailable()"
+      table->reserve();                     // 10. if table is available, we access reserve() method to reserve a table with "table->reserve()"
+      activeCustomers.push_back(customer);  // 11. adding customer to "activeCustomers" vector.
+      std::cout << "Table successfully reserved." << std::endl;
+      return true;                          // returning true if reservation went through.
+    }
+  }
+  // After all tables checked, if tables are not available, adding customer to waitList vector.
+  waitList.push_back(customer);
+  std::cout << "No tables are available at the moment, customer moved to wait-list." << std::endl;
+  return false;                  // returnning false if reservation was not possible at the moment.
+}
