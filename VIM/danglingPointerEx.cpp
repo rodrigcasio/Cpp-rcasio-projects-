55857eb5bb3a091bd2c90d33b7631cf0dd6b9436
rod <rod.casio@outlook.com>
2025-06-22 04:11:03 -0600

    Updated danglingPointerEx.cpp by adding more examples of smart_ptr owned address
    ADded another example of outputting the address of the object variable owned by the pointer and the address of the pointer itself with the reference operator '&'

--- VIM/danglingPointerEx.cpp
+++ VIM/danglingPointerEx.cpp
@@ -3,15 +3,29 @@
 // Example of a dangling pointer, we avoid dereferencing dangling pointers
 // by setting them "nullptr" after deleting them.
 
+#if __cplusplus <= 201103L
+namespace std {
+    template <typename T, typename... Args>
+    std::unique_ptr<T> make_unique(Args&&... args) {
+        return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+    }
+}
+#endif
+
 int main(){
     
     int* ptr1 = new int(77);      // Raw pointer
-
+    std::unique_ptr<int> ptr2 = std::make_unique<int>(30);
+    
     
     std::cout << "address of the int variable: (ptr1) " << ptr1 <<std::endl 
               << "address of the ptr variable: (&ptr) "<< &ptr1 << std::endl 
               << "the value of the integer getting pointed: (*ptr1) "<< *ptr1 << std::endl;
     
+    std::cout << "The address of the integer object on the heap: (ptr2) " << ptr2 << std::endl;
+    std::cout << "The address of the smart_ptr variable itsef: (ptr2) " << &ptr2 << std::endl;   
+
+
     delete ptr1;                
     
     ptr1 = nullptr;
