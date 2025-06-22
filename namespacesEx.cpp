a8cda51a005960cc49481bd2b628f0450a4a03b1
rod <rod.casio@outlook.com>
2025-06-22 17:16:10 -0600

    Updated namespaceEx.cpp with a explanation comment

--- namespacesEx.cpp
+++ namespacesEx.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "h_NamespacesEx.hpp"
+
+// this is an example of accessing a integer variable from the inside of a namespace called 'AppConfig'
 int main(){
 
     std::cout << "The maximum amount of riders in cars is "
