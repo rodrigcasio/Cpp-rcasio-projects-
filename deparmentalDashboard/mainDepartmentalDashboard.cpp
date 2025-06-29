f95a629eed47cd00781e730c90e79d362f9510ce
rod <rod.casio@outlook.com>
2025-06-29 17:22:07 -0600

    Updated file with output separation for each void method used

--- deparmentalDashboard/mainDepartmentalDashboard.cpp
+++ deparmentalDashboard/mainDepartmentalDashboard.cpp
@@ -3,6 +3,8 @@
 #include "h_SalesDepartmentalDashboard.hpp"
 
 int main(){
+
+    namespace audit = Marketing::Support;   // 26.
     
     std::vector<std::string> marketingMembers; // 18.
     std::vector<std::string> salesMembers;  // 18.1
@@ -15,13 +17,32 @@ int main(){
     marketingTeam.addMember("Nicole");
     marketingTeam.addMember("Artem");
 
+std::cout << "---" << std::endl;
+
     Sales::Team salesTeam("Stellar Corp", salesMembers, "London"); // 21. Creating team   
 
     salesTeam.addTeamMember("Pratik"); // 22. Adding members into the team
     salesTeam.addTeamMember("Emily");
     salesTeam.addTeamMember("Clive");
 
-    salesTeam.printTeamDetails();
+std::cout << "---" << std::endl;
+
+    salesTeam.printTeamDetails();   // 23.
+
+std::cout << "---" << std::endl;
+
+    // moving the sales team to a different location.
+    salesTeam.setTeamLocation("New York");  // 24.
+
+    // checking if change was made successfully
+    salesTeam.printTeamDetails();   // 25.
+
+std::cout << "---" << std::endl;
+   
+    // using the alias 'audit' to access the function from a nested namespace called Support within the Marketing namespace
+    audit::auditTeam(marketingTeam);    // 27. must use the team object created
+
+std::cout << "---" << std::endl;
 
     return 0;
 }
