9a22010ed25a3db2608fc9c966a1e384b8e89060
rod <rod.casio@outlook.com>
2025-07-15 14:31:40 -0600

    Updated Packet class" "
    asd
    ""
    "

--- NetworkPacketProcessingProject/fns_Packet.cpp
+++ NetworkPacketProcessingProject/fns_Packet.cpp
@@ -1,1 +1,21 @@
+#include "h_Packet.hpp"
+#include <string>
+#include <iostream>
 
+Packet::Packet(PacketType pType, Protocol ptrcl): type(pType), protocol(ptrcl){ // 8.
+
+}
+
+const std::string Packet::getProtocolName() const{  
+    switch(protocol){   // 9. 
+        case Protocol::TCP: return "TCP"; break;    // 10.
+        case Protocol::UDP: return "UDP"; break;
+        case Protocol::HTTP: return "HTTP"; break;
+        case Protocol::FTP: return "FTP"; break;
+        default: return "Unknown Protocol";       
+    }
+}
+
+void Packet::setType(PacketType newType){   // 5.
+    type = newType;
+}
--- NetworkPacketProcessingProject/h_Packet.hpp
+++ NetworkPacketProcessingProject/h_Packet.hpp
@@ -6,12 +6,17 @@
 class Packet {
     private:
         
-        PacketType type;
+        PacketType type;    // 4.
+        Protocol protocol;  // 6.
+        Status packetStatus; // 7.
 
         const std::string getPacketTypeName() const;
         const std::string getStatusName() const;
         
     public:
+        Packet(PacketType pType, Protocol ptrcl);
+
+        void setType(PacketType newType);
         
         const std::string getProtocolName() const;
         void validatePacket();
