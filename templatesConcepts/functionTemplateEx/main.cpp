5e4d487c5bb375b4efa26759faaa50635bdf75c4
rod <rod.casio@outlook.com>
2025-07-17 15:17:25 -0600

    Updated code indentation for template example #2

--- templatesConcepts/functionTemplateEx/main.cpp
+++ templatesConcepts/functionTemplateEx/main.cpp
@@ -24,7 +24,7 @@ int main(){
     bool result3 = areNumbersEqual(4, 3.8); 
     bool result4 = areNumbersEqual<int, double>(3, 92.23);
 
-    std::cout << "Is is 3.2 equal to 7.9?: " << result3 << std::endl;
+    std::cout << "Is is 3.2 equal to 7.9?: " << std::to_string(result3) << std::endl;
     std::cout << "Is 3 equal to 92.23?: " << result4 << std::endl;
 
     return 0;
